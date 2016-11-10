01ed53f0d1f19ad254e8c845a4463ef63b96d0ba
Conlan <cjhanwel@ucalgary.ca>
2016-11-10 14:17:33 -0700

    initial commit

---
+++ src/code/UserInterface.java
@@ -0,0 +1,17 @@
+package code;
+
+public class UserInterface {
+
+	public UserInterface() {
+		
+	}
+	
+	public void displayStart() {
+		System.out.print("Please select the type of Object you wish to serialize:\n"
+				+ "1) Primitive Only Object\n"
+				+ "2) Reference Object\n"
+				+ "3) Primitive Array Object\n"
+				+ "4) Reference Array Object\n");
+	}
+	
+}
---
+++ src/obj/PrimClass1.java
@@ -0,0 +1,20 @@
+package obj;
+
+public class PrimClass1 {
+	private int i;
+	//private char c;
+	//private boolean b;
+	
+	public PrimClass1 () {
+		
+	}
+	
+	public int getI() {
+		return i;
+	}
+	
+	public void setI(int in) {
+		i = in;
+	}
+
+}
